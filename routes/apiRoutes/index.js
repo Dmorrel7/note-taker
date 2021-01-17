26388cf528fbcaa0d3c143fa20346d89956b2479
Dalton Morrel <daltonmorrel7&@gmail.com>
2021-01-17 18:24:51 -0500

    Add Heroku

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,31 @@
+const router = require("express").Router()
+const fs = require("fs");
+const createNewNote = require("../../lib/notes");
+
+
+const notes = require("../../db/db");
+
+router.get("/notes", (req, res) => 
+{
+    console.log(notes)
+
+    let results = notes;
+    if (req.query) 
+    {
+      res.json(results);
+    }
+
+});
+
+router.post("/notes", (req, res) => 
+{
+
+    req.body.id = notes.length.toString();
+
+    const newNote = createNewNote(req.body, notes)
+
+   res.json(newNote)
+})
+
+
+module.exports = router 
